422331a877febc02727c459f5179baad30b1069f
sardarjii12 <148244550+sardarjii12@users.noreply.github.com>
2023-10-24 18:27:04 +0000

    bb

--- day7/day7.js
+++ day7/day7.js
@@ -1,2 +1,48 @@
-let myArey =[1,2,3,4,5,6,7]
-console.log(myArey[5])
+// let mydate = new Date
+// console.log(mydate.toString)
+// // console.log(mydate.toDateString)
+// // console.log(mydate.toDateString)
+// console.log(mydate)
+
+// let mynewate = new Date(2023, 0, 45)
+// console.log(mynewate.toLocaleDateString);
+// let mytime = Date.now()
+// console.log(mytime.getDate());
+ const number = [0, 1, 2, 3, 4, 5]
+
+// console.log(number[3]);
+
+// console.log(number.flat(7));
+// number.push(8)
+// number.pop()
+// number.unshift(4)
+// number.shift()
+
+// console.log(number)
+// // console.log(number.includes(5))
+// // console.log(number.indexOf(9))
+// const thiis = number.join()
+// console.log(thiis)
+// const myary1 = number.slice(1, 5)
+// const myary12 = number.splice(1, 5)
+// console.log(myary1)
+// console.log(myary12)
+
+
+
+
+
+// 2nd class
+
+
+// const myclass =[1, 2, 3]
+// const myclass2 =[4, 5, 6]
+// const  newclass =[ myclass.concat(myclass2)]
+// const neclass1 = [...myclass, ...myclass2]
+// console.log(myclass);
+// console.log(newclass);
+// console.log(neclass1)
+const a_Arry = [1, 2, 3, [5, 6, 7], 8, 9,[5, 6, 8, [7, 5, 6,]]]
+let  b_eArry = a_Arry.flat(isFinite)
+// console.log(a_Arry);
+console.log(b_eArry);
